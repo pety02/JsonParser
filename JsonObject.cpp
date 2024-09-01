fd5698b8170da3489b712a2a233a355f1b7b1a00
pety02 <pety02@abv.bg>
2024-09-01 15:07:23 +0300

    JsonObject class documentted

--- JsonObject.cpp
+++ JsonObject.cpp
@@ -24,9 +24,8 @@ void JsonObject::destroy()
     delete this->next;
 }
 
-/// @brief 
-/// @param json 
-/// @return 
+/// @brief Constructor with a string in json format
+/// @param json a string in json format that represent an object
 /// @author Petya Licheva - pety02
 JsonObject::JsonObject(const std::string& json)
 {
@@ -78,15 +77,16 @@ JsonObject::JsonObject(const std::string& json)
     }
 }
 
-/// @brief 
-/// @param type 
-/// @param key 
-/// @param value 
-/// @param children 
-/// @param next 
-/// @return
+/// @brief Constructor with all arguments
+/// @param type the valueType of the JsonObject
+/// @param key the key of the JsonObject
+/// @param value the value of the JsonObject
+/// @param children the nested JsonObjects 
+///                 (children of the current JsonObject)
+/// @param next the next JsonObject
 /// @author Petya Licheva - pety02
-JsonObject::JsonObject(JsonValueType type, const std::string &key, const std::string &value, std::vector<JsonObject*> children, JsonObject *next)
+JsonObject::JsonObject(JsonValueType type, const std::string &key, const std::string &value, 
+    std::vector<JsonObject*> children, JsonObject *next)
     : valueType(type), key(key), value(value), next(next), children(std::vector<JsonObject*>())
 {
     for(int i = 0; i < children.size(); ++i) {
@@ -94,18 +94,17 @@ JsonObject::JsonObject(JsonValueType type, const std::string &key, const std::st
     }
 }
 
-/// @brief 
-/// @param other 
-/// @return 
+/// @brief Copy constructor of JsonObject
+/// @param other the copied JsonObject
 /// @author Petya Licheva - pety02
 JsonObject::JsonObject(const JsonObject &other)
 {
     this->copy(other);
 }
 
-/// @brief 
-/// @param other 
-/// @return 
+/// @brief Assignment operator of JsonObject
+/// @param other the assigned JsonObject
+/// @return a refernce to current JsonObject
 /// @author Petya Licheva - pety02
 JsonObject &JsonObject::operator=(const JsonObject &other)
 {
@@ -118,51 +117,66 @@ JsonObject &JsonObject::operator=(const JsonObject &other)
     return *this;
 }
 
-/// @brief 
-/// @param left 
-/// @param right 
-/// @return 
+/// @brief Operator== for comparison of JsonObjects
+/// @param left the left JsonObject
+/// @param right the right JsonObject
+/// @return true if the left JsonObject is equal to the 
+///         right JsonObject and false if not
 /// @author Petya Licheva - pety02
 bool operator==(const JsonObject &left, const JsonObject &right)
 {
-    return left.key == right.key && left.value == right.value;
+    bool areEqual = left.valueType == right.valueType && left.key == right.key 
+        && left.value == right.value && left.next == right.next;
+    
+    if(!areEqual || left.children.size() != right.children.size()) {
+        return false;
+    }
+    for(int i = 0; i < left.children.size(); ++i) {
+        if(left.children[i] != right.children[i]) {
+            areEqual = false;
+            break;
+        }
+    }
+
+    return areEqual;
 }
 
-/// @brief 
-/// @param left 
-/// @param right 
-/// @return 
+/// @brief Operator== for comparison of JsonObjects
+/// @param left the left JsonObject
+/// @param right the right JsonObject
+/// @return false if the left JsonObject is equal to the 
+///         right JsonObject and true if not
 /// @author Petya Licheva - pety02
 bool operator!=(const JsonObject &left, const JsonObject &right)
 {
     return left.key != right.key && left.value != right.value;
 }
 
-/// @brief 
+/// @brief Destroys the current JsonObject
 /// @author Petya Licheva - pety02
 JsonObject::~JsonObject()
 {
     this->destroy();
 }
 
-/// @brief 
-/// @param newNext 
+/// @brief a method that sets the next JsonObject
+/// @param newNext the new next JsonObject
 /// @author Petya Licheva - pety02
 void JsonObject::setNext(const JsonObject &newNext)
 {
     this->next = new JsonObject(newNext.getType(), newNext.getKey(), newNext.getValue(), newNext.getChildren(), newNext.getNext());
 }
 
-/// @brief 
-/// @param node 
+/// @brief a method that adds to the chain a new JsonObject node
+/// @param node the new JsonObject node
 /// @author Petya Licheva - pety02
 void JsonObject::add(const JsonObject &node)
 {
     this->children.push_back(new JsonObject(node));
 }
 
-/// @brief 
-/// @param node 
+/// @brief a method that removes from the chain a definite JsonObject node
+/// @param node the definte JsonObject node
 /// @author Petya Licheva - pety02
 void JsonObject::remove(const JsonObject &node)
 {
@@ -176,40 +190,40 @@ void JsonObject::remove(const JsonObject &node)
     }
 }
 
-/// @brief 
-/// @return 
+/// @brief a method that returns the valueType of the current JsonObject
+/// @return the valueType of the current JsonObject
 /// @author Petya Licheva - pety02
 JsonValueType JsonObject::getType() const
 {
     return this->valueType;
 }
 
-/// @brief 
-/// @return 
+/// @brief a method that returns the key of the current JsonObject
+/// @return the key of the current JsonObject
 /// @author Petya Licheva - pety02
 std::string JsonObject::getKey() const
 {
     return this->key;
 }
 
-/// @brief 
-/// @return 
+/// @brief a method that returns the value of the current JsonObject
+/// @return the value of the current JsonObject
 /// @author Petya Licheva - pety02
 std::string JsonObject::getValue() const
 {
     return this->value;
 }
 
-/// @brief 
-/// @return
+/// @brief a method that returns the next of the current JsonObject
+/// @return the next of the current JsonObject
 /// @author Petya Licheva - pety02 
 JsonObject *JsonObject::getNext() const
 {
     return this->next;
 }
 
-/// @brief 
-/// @return 
+/// @brief a method that returns the children vector of the current JsonObject
+/// @return the children vector of the current JsonObject 
 /// @author Petya Licheva - pety02
 std::vector<JsonObject *> JsonObject::getChildren() const
 {
